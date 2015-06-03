dfc0f95374990b54f8646e2416d67039ff66eae0
baker@BALLOONGA <yamada.masahiro@gmail.com>
2015-06-03 22:41:31 +0900

    add search field for Account

--- app/controllers/casein/accounts_controller.rb
+++ app/controllers/casein/accounts_controller.rb
@@ -9,7 +9,7 @@ module Casein
   
     def index
       @casein_page_title = 'Accounts'
-  		@accounts = Account.order(sort_order(:name)).paginate :page => params[:page]
+  		@accounts =  Account.search(params[:search]).order(sort_order(:name)).paginate :page => params[:page]
     end
   
     def show
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,10 @@
 class Account < ActiveRecord::Base
   
+	def self.search(search)
+		if search
+			Account.where(['name LIKE ?', "%#{search}%"])
+		else
+			Account.all
+		end
+	end
 end
