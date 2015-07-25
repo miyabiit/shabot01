f7ec12d582aea838d48ffaa7c3b4fd004a1f58e8
yamada.masahiro <yamada@shallontec.jp>
2015-07-25 13:09:37 +0900

    update for replacing shallontec site

--- app/controllers/casein/projects_controller.rb
+++ app/controllers/casein/projects_controller.rb
@@ -9,7 +9,7 @@ module Casein
   
     def index
       @casein_page_title = 'Projects'
-  		@projects = Project.search(params[:search]).order(sort_order(:name)).paginate :page => params[:page]
+  		@projects = Project.search(params[:search]).order(sort_order(:name)).order(sort_order(:category)).paginate :page => params[:page]
     end
   
     def show
