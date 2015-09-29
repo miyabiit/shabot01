91109448800241cce064f286fb70ebd5ae9d6e5f
baker@BALLOONGA <yamada.masahiro@gmail.com>
2015-09-29 14:27:38 +0900

    update report (receipt

--- app/pdfs/payment_report.rb
+++ app/pdfs/payment_report.rb
@@ -58,6 +58,7 @@ class PaymentReport < Prawn::Document
 		fill_color "0000ff"
 		draw_text @payment.org_name , :size => 12, :at => [5,760]
 		fill_color "000000"
+		draw_text '作成:' + Date.today.strftime("%Y/%m/%d") , :size => 10, :at => [420,760]
 		draw_text "支払申請書", :size => 18, :at => [5,730]
 		draw_text "No." + @payment.slip_no.to_s , :size => 12, :at => [5,710]
 
